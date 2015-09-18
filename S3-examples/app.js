37b4c4330dd12f37e06681e58c5104b443c46905
Calvin Lee <cfernandes@loyalty.com>
2015-09-18 00:18:09 -0400

    Update app.js
    fixed comments

--- S3-examples/app.js
+++ S3-examples/app.js
@@ -11,32 +11,32 @@ function print(err, data) {
     }
 }
 
-// it all starts with creating a bucket
+// It all starts with creating a bucket
 s3Wrapper.createBucket(
     'calvin-hello-bucket',
     (err, data) => {
-        // callback unsuccessful
+        // Callback unsuccessful
         if (err) {
             console.log('Error: ' + err);
         } else {
-            // successful callback (meaning the bucket is created)
+            // Successful callback (meaning the bucket is created)
             console.log(data);
 
-            //create a key with value as text
+            // Create a key with value as text
             s3Wrapper.uploadKeyValuePairToBucket('hello', 'there', 'calvin-hello-bucket', (err, data) => {
                 if (err) {
                     console.log(err);
                 }
                 else {
-                    // since we were able to successfully store the key (hello), let's get it back and put it in a file
+                    // Since we were able to successfully store the key (hello), let's get it back and put it in a file
                     s3Wrapper.downloadFileToPathFromBucket('hello', 'hello.txt', 'calvin-hello-bucket');
                 }
             });
 
-            //create a key with value coming from a file
+            // Create a key with value coming from a file
             s3Wrapper.uploadFileIdentifiedWithKeyToBucket('config', './package.json', 'calvin-hello-bucket', print);
 
-            //Put a timer on emptying and deleting the bucket
+            // Put a timer on emptying and deleting the bucket
             setTimeout(
                 () => {
                     s3Wrapper.listKeys({Bucket: 'calvin-hello-bucket'}, print);
